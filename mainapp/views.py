f5dea4a2a6afd219466de64f9b7e1eb9f7871d0f
OllyYa <exrhodo@gmail.com>
2022-10-05 22:18:07 +0300

    lesson-3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,37 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from datetime import datetime
 
 
-def hello_view(request):
-    return HttpResponse("Hello world")
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['news_title'] = 'Новость'
+        context['description'] = 'Предварительное описание новости'
+        context['news_date'] = datetime.now()
+        context['range'] = range(5)
+
+        return context
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
